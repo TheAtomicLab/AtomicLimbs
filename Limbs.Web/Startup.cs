729b8a48ef503dcc5d7eac366d755058dd09a969
aleban <aleban@MININT-PD4NT1E>
2016-08-27 15:06:45 -0300

    Add project files.

---
+++ Limbs.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Limbs.Web.Startup))]
+namespace Limbs.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
