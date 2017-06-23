053cd9adf6e55fdef587e0d7ca074f1a5124f7e8
Agustin PC <agusfn20@gmail.com>
2017-06-22 22:56:43 -0300

    Agregadas Actions para aceptar y rechazar (siendo embajador) asignaciones de pedidos. Actualizadas vistas.

--- Limbs.Web/Controllers/OrdersController.cs
+++ Limbs.Web/Controllers/OrdersController.cs
@@ -8,6 +8,8 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Limbs.Web.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
 
 namespace Limbs.Web.Controllers
 {
@@ -15,11 +17,84 @@ namespace Limbs.Web.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: Orders
+
+        // GET: Orders/Index
+        [Authorize]
         public async Task<ActionResult> Index()
         {
-            return View(await db.OrderModels.ToListAsync());
+
+            if (User.IsInRole("Usuario"))
+            {
+                return View("ListaUsuario");
+            }
+            else if (User.IsInRole("Embajador"))
+            {
+
+                var currentUserId = User.Identity.GetUserId();
+                var orderList = await db.OrderModels.Where(m => m.OrderUser.Id == currentUserId).ToListAsync();
+
+
+                var pendingAssignationList = orderList.Where(o => o.Status == OrderStatus.NotAssigned).OrderByDescending(m => m.Id);
+                var assignedList = orderList.Where(o => o.Status != OrderStatus.NotAssigned).OrderByDescending(m => m.Id);
+
+                var viewModel = new ViewModels.EmbajadorOrderListViewModel()
+                {
+                    PendingToAssignOrders = pendingAssignationList,
+                    AssignedOrders = assignedList
+                };
+
+                return View("ListaEmbajador", viewModel);
+            }
+            else // Admin?
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
         }
+
+
+        [Authorize(Roles = "Embajador")]
+        public ActionResult AcceptAssignation(int? Id)
+        {
+            var orderInDb = db.OrderModels.SingleOrDefault(m => m.Id == Id);
+            if (orderInDb == null) return HttpNotFound();
+
+            // Comprobamos que el pedido esté asignado al usuario y esté pendiente de asignacion
+            if (orderInDb.OrderUser.Id == User.Identity.GetUserId() && orderInDb.Status == OrderStatus.NotAssigned)
+            {
+                orderInDb.Status = OrderStatus.Pending;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+
+        }
+
+        [Authorize(Roles = "Embajador")]
+        public ActionResult RejectAssignation(int? Id)
+        {
+            var orderInDb = db.OrderModels.SingleOrDefault(m => m.Id == Id);
+            if (orderInDb == null) return HttpNotFound();
+
+            // Comprobamos que el pedido esté asignado al usuario y esté pendiente de asignacion
+            if (orderInDb.OrderUser.Id == User.Identity.GetUserId() && orderInDb.Status == OrderStatus.NotAssigned)
+            {
+                // Rechazar.
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+
+        }
+
+
+
 
         // GET: Orders/Details/5
         public async Task<ActionResult> Details(int? id)
@@ -46,15 +121,27 @@ namespace Limbs.Web.Controllers
         {
             return View("pedir_mano_inicio");
         }
+
         // POST: Orders/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
+        [Authorize(Roles = "Usuario")]
         public async Task<ActionResult> Create([Bind(Include = "Id,Design,Sizes,Comments")] OrderModel orderModel)
         {
             if (ModelState.IsValid)
             {
-                orderModel.Status = OrderStatus.Pending;
+
+                // Asignar usuario que genero la orden (hay mejor manera pero es de prueba)
+                var userId = User.Identity.GetUserId();
+                orderModel.OrderRequestor = db.Users.SingleOrDefault(m => m.Id == userId);
+
+                // ---- TEMPORAL --- Asignar al user embajador@limbs.com el manejo de la orden
+                orderModel.OrderUser = db.Users.SingleOrDefault(m => m.Email == "embajador@limbs.com");
+
+
+                orderModel.Status = OrderStatus.NotAssigned;
                 db.OrderModels.Add(orderModel);
                 await db.SaveChangesAsync();
                 return RedirectToAction("Index");
---
+++ Limbs.Web/ViewModels/EmbajadorOrderListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Limbs.Web.Models;
+
+namespace Limbs.Web.ViewModels
+{
+    public class EmbajadorOrderListViewModel
+    {
+        public IEnumerable<OrderModel> PendingToAssignOrders { get; set; }
+        public IEnumerable<OrderModel> AssignedOrders { get; set; }
+    }
+}
