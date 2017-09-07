b40e15fd9d63ce93250839493cc18f7d2ba06b85
Alejandro Banzas <alebanzas@outlook.com>
2017-09-07 18:39:13 -0300

    starting iUserFiles

---
+++ Limbs.Web/Repositories/Interfaces/IUserFiles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Limbs.Web.Repositories.Interfaces
+{
+    interface IUserFiles
+    {
+
+        Uri UploadOrderFile(Stream file, string name);
+
+    }
+}
