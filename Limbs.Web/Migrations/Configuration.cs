f2ef08976bfec3cd70bb87acb234784e06317f84
Micaela Yanigro <micayanigro@gmail.com>
2017-09-10 18:51:29 -0300

    Modificacion en metodo SEED para que solo agregue los roles si la tabla está vacia.

--- Limbs.Web/Migrations/Configuration.cs
+++ Limbs.Web/Migrations/Configuration.cs
@@ -2,6 +2,7 @@ namespace Limbs.Web.Migrations
 {
     using Limbs.Web.Models;
     using System.Data.Entity.Migrations;
+    using System.Linq;
 
     public sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
@@ -12,12 +13,16 @@ namespace Limbs.Web.Migrations
 
         protected override void Seed(ApplicationDbContext context)
         {
-            context.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole("Unassigned"));
-            context.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole("Requester"));
-            context.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole("Ambassador"));
-            context.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole("Admin"));
-            
-            context.SaveChanges();
+
+            if (context.Roles.Count() == 0)
+            {
+                context.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole("Unassigned"));
+                context.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole("Requester"));
+                context.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole("Ambassador"));
+                context.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole("Admin"));
+
+                context.SaveChanges();
+            }
 
         }
     }
