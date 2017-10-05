21556162b9e1a4b739e92fc983c9c7e19c88dd44
Alejandro Banzas <alebanzas@outlook.com>
2017-10-04 23:25:07 -0300

    Order Notification service implementation

--- Limbs.Web/Services/OrderMailNotificationService.cs
+++ Limbs.Web/Services/OrderMailNotificationService.cs
@@ -1,34 +1,131 @@
-﻿using System.Threading.Tasks;
+﻿using System.Configuration;
+using System.Threading.Tasks;
 using Limbs.Web.Models;
+using Limbs.Web.Storage.Azure.QueueStorage;
+using Limbs.Web.Storage.Azure.QueueStorage.Messages;
 
 namespace Limbs.Web.Services
 {
     public class OrderMailNotificationService : IOrderNotificationService
     {
+        private readonly string _fromEmail = ConfigurationManager.AppSettings["Mail.Username"];
+        private readonly string _adminEmails = ConfigurationManager.AppSettings["AdminEmails"];
+
         public async Task SendStatusChangeNotification(OrderModel order, OrderStatus oldStatus, OrderStatus newStatus)
         {
-            //TODO (ale): implement
-            return;
+            if (oldStatus == newStatus) return;
+
+            switch (newStatus)
+            {
+                case OrderStatus.NotAssigned:
+                    //No entra nunca
+                    break;
+                case OrderStatus.PreAssigned:
+                    //Entra por SendAmbassadorChangedNotification
+                    break;
+                case OrderStatus.Pending:
+                    //TODO: avisar que esta en proceso de impresion
+
+                    var mailMessage = new MailMessage
+                    {
+                        From = _fromEmail,
+                        Subject = $"[Atomic Limbs] Solicitud de orden (orden {order.Id})",
+                        To = "",
+                        Body = "", //TODO (ale): segun template
+                    };
+
+                    await AzureQueue.EnqueueAsync(mailMessage);
+
+                    break;
+                case OrderStatus.Ready:
+
+                    mailMessage = new MailMessage
+                    {
+                        From = _fromEmail,
+                        Subject = $"[Atomic Limbs] Coordinar envío (orden {order.Id})",
+                        To = _adminEmails,
+                        Body = "", //TODO (ale): segun template
+                    };
+                    await AzureQueue.EnqueueAsync(mailMessage);
+
+                    mailMessage = new MailMessage
+                    {
+                        From = _fromEmail,
+                        Subject = $"[Atomic Limbs] Coordinar envío (orden {order.Id})",
+                        To = order.OrderAmbassador.Email,
+                        Body = "", //TODO (ale): segun template
+                    };
+                    await AzureQueue.EnqueueAsync(mailMessage);
+
+                    mailMessage = new MailMessage
+                    {
+                        From = _fromEmail,
+                        Subject = $"[Atomic Limbs] Pedido listo (orden {order.Id})",
+                        To = order.OrderAmbassador.Email,
+                        Body = "", //TODO (ale): segun template
+                    };
+                    await AzureQueue.EnqueueAsync(mailMessage);
+                    
+                    break;
+                case OrderStatus.Delivered:
+                    //Entra por SendDeliveryInformationNotification
+                    break;
+            }
         }
-        
+
         public async Task SendAmbassadorChangedNotification(OrderModel order, AmbassadorModel oldAmbassador, AmbassadorModel newAmbassador)
         {
-            //TODO (ale): implement
-            return;
+            var mailMessage = new MailMessage
+            {
+                From = _fromEmail,
+                Subject = $"[Atomic Limbs] Solicitud de orden (orden {order.Id})",
+                To = newAmbassador.Email,
+                Body = "" //TODO (ale): segun template
+            };
+
+            await AzureQueue.EnqueueAsync(mailMessage);
+
+            if (oldAmbassador != null && oldAmbassador != newAmbassador) //tenia otro embajador
+            {
+                mailMessage = new MailMessage
+                {
+                    From = _fromEmail,
+                    Subject = $"[Atomic Limbs] Cambio de embajador (orden {order.Id})",
+                    To = oldAmbassador.Email,
+                    Body = "" //TODO (ale): segun template
+                };
+
+                await AzureQueue.EnqueueAsync(mailMessage);
+            }
+
         }
 
         public async Task SendDeliveryInformationNotification(OrderModel order)
         {
-            //TODO (ale): implement
-            return;
+            var mailMessage = new MailMessage
+            {
+                From = _fromEmail,
+                Subject = $"[Atomic Limbs] Información de envío (orden {order.Id})",
+                To = order.OrderAmbassador.Email,
+                Cc = order.OrderRequestor.Email,
+                Body = "" //TODO (ale): segun template
+            };
 
+            await AzureQueue.EnqueueAsync(mailMessage);
         }
 
         public async Task SendProofOfDeliveryNotification(OrderModel order)
         {
-            //TODO (ale): implement
-            return;
+            var mailMessage = new MailMessage
+            {
+                From = _fromEmail,
+                Subject = $"[Atomic Limbs] Nueva prueba de entrega (orden {order.Id})",
+                To = order.OrderAmbassador.Email,
+                Cc = order.OrderRequestor.Email,
+                Body = "" //TODO (ale): segun template
+            };
 
+            await AzureQueue.EnqueueAsync(mailMessage);
         }
     }
 }
