131d7b2afa9b1214662dd302b5de7fcc606e7ff4
Alejandro Banzas <alebanzas@outlook.com>
2017-10-29 20:37:56 +0100

    Password criteria w/lower complexity

--- Limbs.Web/App_Start/IdentityConfig.cs
+++ Limbs.Web/App_Start/IdentityConfig.cs
@@ -53,11 +53,11 @@ namespace Limbs.Web
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
+                RequiredLength = 8,
                 RequireNonLetterOrDigit = true,
-                RequireDigit = true,
-                RequireLowercase = true,
-                RequireUppercase = true,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireUppercase = false,
             };
 
             // Configure user lockout defaults
