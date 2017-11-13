f9b5f876426fada81c242d52f57cb3a3f292c1f7
Alejandro Banzas <alebanzas@outlook.com>
2017-11-12 22:42:12 -0300

    Error messages in resource file

--- Limbs.Web/Startup.cs
+++ Limbs.Web/Startup.cs
@@ -1,4 +1,6 @@
-﻿using LightInject;
+﻿using System;
+using System.Reflection;
+using LightInject;
 using Limbs.Web.Common.Mail;
 using Limbs.Web.Models;
 using Limbs.Web.Repositories;
@@ -16,11 +18,21 @@ namespace Limbs.Web
     {
         public void Configuration(IAppBuilder app)
         {
+            ConfigureLocalization();
             ConfigureAuth(app);
             ConfigureServices(app);
 
             FullStorageInitializer.Initialize();
             MailTemplatesRegistration.Initialize();
+        }
+
+        private void ConfigureLocalization()
+        {
+            var ass = Assembly.Load("Microsoft.AspNet.Identity.Core, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
+            Type hack = ass.GetType("Microsoft.AspNet.Identity.Resources");
+            var field = hack.GetField("resourceMan", BindingFlags.Static | BindingFlags.NonPublic);
+            //This is where you set your own local resource manager that will read resource files from your own assembly
+            field.SetValue(null, new global::System.Resources.ResourceManager("Limbs.Web.Properties.Resources", typeof(Limbs.Web.Properties.Resources).Assembly));
         }
 
         public void ConfigureServices(IAppBuilder app)
