e5023b06eca572b13b644c36ff8c2b514f5ed50a
Alejandro Banzas <alebanzas@outlook.com>
2017-11-20 21:40:23 -0300

    Product generator working

--- Limbs.Functions/index.js
+++ Limbs.Functions/index.js
@@ -3,54 +3,70 @@ var azure = require('azure-storage');
 var fs = require('fs');
 
 var AzureFunction = function (context, myQueueItem) {
-    
-    var messageData = JSON.parse(myQueueItem);
-    
-    var script = 'function main() { return [ torus() ] }';
-    var params = {}
 
-    context.log("compiling with JSCad");
-    jscad.compile(script, params).then(function (compiled) {
-        context.log("OK compiling with JSCad");
+    try {
 
-        var outputData = jscad.generateOutput('stlb', compiled);
-        var fileName = "product-file-" + new Date().getMilliseconds() + ".stl";
-        
-        fs.writeFileSync(fileName, outputData.asBuffer());
-        fs.readFile(fileName, function (err, data) {
-            var bs = azure.createBlobService();
+        var messageData = JSON.parse(JSON.stringify(myQueueItem));
+        var fileName = "product-" + messageData.OrderId + "-file-" + new Date().getMilliseconds() + ".stl";
+        messageData.FileUrl = fileName;
+
+        //RENDER JSCAD MODEL//////////////////////////////////////////////////////////
+        var script = 'function main() { return [ torus() ] }';
+        var params = {}
+
+        context.log("compiling with JSCad");
+        jscad.compile(script, params).then(function (compiled) {
+            context.log("OK compiling with JSCad");
+            
+            context.log("generating output with JSCad");
+            var outputData = jscad.generateOutput('stlb', compiled);
+            context.log("OK generating output with JSCad");
+
+            //TEMPORAL COMPILED FILE//////////////////////////////////////////////////////
+
+            context.log("saving temp file lo local fs");
+            fs.writeFileSync(getUserHome() + fileName, outputData.asBuffer());
+            context.log("OK saving temp file lo local fs");
+
+            context.log("read file from local fs");
+            var data = fs.readFileSync(getUserHome() + fileName);
+            context.log("OK read file from local fs");
+
+            //UPLOAD TO BLOB STORAGE//////////////////////////////////////////////////////
+
             context.log("uploading file to Azure");
-            bs.createBlockBlobFromText("orderproductgenerated", fileName, data, function (error, result, response) {
-                if (error) {
-                    context.log("ERROR uploading file to Azure " + fileName);
-                    context.log(error);
-                    return;
-                }
-                context.log("OK uploading file to Azure");
+            var bs = azure.createBlobService();
+            bs.createBlockBlobFromText("orderproductgenerated", fileName, data, function (e) { return e; });
+            context.log("OK uploading file to Azure");
 
-                messageData.FileUrl = fileName;
+            //NOTIFY THE NEW COMPILED FILE////////////////////////////////////////////////
 
-                var retryOperations = new azure.ExponentialRetryPolicyFilter();
-                var queueSvc = azure.createQueueService().withFilter(retryOperations);
-                queueSvc.createMessage('orderproductgeneratorresult', new Buffer(JSON.stringify(messageData)).toString('base64'), function (err) {
-                    if (!err) {
-                        context.log("OK sending message to queue");
-                    } else {
-                        context.log("ERROR sending message to queue");
-                    }
-                });
-                context.log("deleting from fs");
-                fs.unlink(fileName, (er) => {
-                    if (er) {
-                        console.log('ERROR deleting from fs' + fileName);
-                        return;
-                    }
-                    console.log('OK deleting from fs');
-                });
-            });
+            context.log("sending message to queue");
+            var retryOperations = new azure.ExponentialRetryPolicyFilter();
+            var queueSvc = azure.createQueueService().withFilter(retryOperations);
+            queueSvc.createMessage('orderproductgeneratorresult', new Buffer(JSON.stringify(messageData)).toString('base64'), function (e) { return e; });
+            context.log("OK sending message to queue");
+
+            //DELETE TEMP FILE////////////////////////////////////////////////////////////
+
+            context.log("deleting from fs");
+            fs.unlinkSync(getUserHome() + fileName);
+            context.log('OK deleting from fs');
+
+            //////////////////////////////////////////////////////////////////////////////
+
+            context.log("OK bye");
+            context.done();
         });
-    });
-    context.done();
+    } catch (e) {
+        context.log("ERROR");
+        context.log(e);
+        context.done(e);
+    } 
 };
+
+function getUserHome() {
+    return process.env.TMP + "\\";
+}
 
 module.exports = AzureFunction;
--- Limbs.Functions/local.js
+++ Limbs.Functions/local.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: 'config.env' });
 var AzureFunction = require('./index');
 
 // Local development queue message
-var myQueueItem = '{"ProductSizes":{"Id":0,"A":0.2,"B":0.3,"C":0.4,"D":0},"OrderId":94,"Pieces":{"AtomicLabCover":true,"FingerMechanismHolder":true,"Fingers":true,"FingerStopper":true,"FingersX1":true,"FingersX2P":true,"Palm":true,"ThumbConnector":true,"Thumb":true,"ThumbClip":true,"ThumbScrew":true,"UpperArm_FingerConnector":true,"UpperArm_PalmConnector":true,"UpperArm_ThumbShortConnector":true,"UpperArm_FingerSlider":true,"UpperArm":true}}';
+var myQueueItem = '{"ProductSizes":{"Id":0,"A":4.32,"B":6.19,"C":5.0,"D":0},"OrderId":13,"Pieces":{"AtomicLabCover":false,"FingerMechanismHolder":false,"Fingers":false,"FingerStopper":false,"FingersX1":false,"FingersX2P":false,"Palm":false,"ThumbConnector":false,"Thumb":false,"ThumbClip":false,"ThumbScrew":false,"UpperArm_FingerConnector":false,"UpperArm_PalmConnector":false,"UpperArm_ThumbShortConnector":false,"UpperArm_FingerSlider":false,"UpperArm":false}}';
 
 // Local development context
 var debugContext = {
