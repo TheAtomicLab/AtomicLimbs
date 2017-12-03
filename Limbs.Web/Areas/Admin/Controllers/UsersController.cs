73c458c40a84c660348a58ff4f6b714e32eed743
Alejandro Banzas <alebanzas@outlook.com>
2017-12-03 15:17:12 -0300

    user list in admin

--- Limbs.Web/Areas/Admin/Controllers/UsersController.cs
+++ Limbs.Web/Areas/Admin/Controllers/UsersController.cs
@@ -1,18 +1,57 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using Limbs.Web.Areas.Admin.Models;
+using Limbs.Web.Entities.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Limbs.Web.Areas.Admin.Controllers
 {
     public class UsersController : AdminBaseController
     {
+        private readonly RoleManager<IdentityRole> _roleMngr;
+        public UsersController()
+        {
+            var roleStore = new RoleStore<IdentityRole>(Db);
+            _roleMngr = new RoleManager<IdentityRole>(roleStore);
+        }
+
         // GET: Admin/Users
         public ActionResult Index()
         {
             //TODO (ale): implementar paginacion, export to excel, ordenamiento
             var userList = Db.UserModelsT.ToList();
             return View(userList);
+        }
+
+        // GET: Admin/Users/Registered
+        public ActionResult Registered()
+        {
+            //TODO (ale): implementar paginacion, export to excel, ordenamiento
+            var userList = Db.Users.Include(x => x.Roles).ToList();
+            var ar = _roleMngr.Roles.ToList();
+
+            var result = userList.Select(user =>
+                {
+                    var roles = user.Roles.Select(x => x.RoleId).Select(r => ar.FirstOrDefault(x => x.Id == r)?.Name).ToList();
+                    if (ConfigurationManager.AppSettings["AdminEmails"].Split(',').ToList().Contains(user.Email)) roles.Add(AppRoles.Administrator);
+                    
+                    return new UserViewModel
+                    {
+                        Email = user.Email,
+                        IsEmailConfirmed = user.EmailConfirmed,
+                        Roles = roles.ToArray(),
+                        LoginProviders = user.Logins.Select(x => x.LoginProvider).ToArray(),
+                    };
+                })
+                .ToList();
+            
+            return View(result);
         }
 
         // GET: Admin/Users/Details/5
---
+++ Limbs.Web/Areas/Admin/Models/UserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Limbs.Web.Areas.Admin.Models
+{
+    public class UserViewModel
+    {
+        public string Email { get; set; }
+
+        public bool IsEmailConfirmed { get; set; }
+
+        public string[] Roles { get; set; }
+
+        public string[] LoginProviders { get; set; }
+    }
+}
