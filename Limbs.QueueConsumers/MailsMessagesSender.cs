3475db31aed073a222127d2f6a252e461af33796
Alejandro Banzas <alebanzas@outlook.com>
2017-12-16 18:10:41 -0300

    From email alias

--- Limbs.QueueConsumers/MailsMessagesSender.cs
+++ Limbs.QueueConsumers/MailsMessagesSender.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Configuration;
+using System.Net.Mail;
 using Limbs.Web.Common.Extensions;
 using Limbs.Web.Common.Mail;
 using Limbs.Web.Storage.Azure.QueueStorage;
-using Limbs.Web.Storage.Azure.QueueStorage.Messages;
+using MailMessage = Limbs.Web.Storage.Azure.QueueStorage.Messages.MailMessage;
 
 namespace Limbs.QueueConsumers
 {
@@ -23,19 +24,26 @@ namespace Limbs.QueueConsumers
 
                 var mailSender = new GeneralMailSender(mailserver, username, password);
                 var mailMessage = new System.Net.Mail.MailMessage(
-                    message.Data.From, message.Data.To,
-                    message.Data.Subject, message.Data.Body) {IsBodyHtml = true};
-                if(!string.IsNullOrWhiteSpace(message.Data.Cc)) mailMessage.CC.Add(message.Data.Cc);
+                    message.Data.From,
+                    message.Data.To,
+                    message.Data.Subject,
+                    message.Data.Body)
+                {
+                    IsBodyHtml = true,
+                    From = new MailAddress(message.Data.From, "Atomic Lab")
+                };
+
+                if (!string.IsNullOrWhiteSpace(message.Data.Cc)) mailMessage.CC.Add(message.Data.Cc);
                 // si el mensaje es null significa que el maker controló algunas situaciones y no hay nada para enviar y el mensaje se puede remover de la queue
                 mailSender.Send(mailMessage);
 
-                Console.WriteLine($"Email sent ({message.Data.Subject}) to: {message.Data.To}");
+                Console.WriteLine($@"Email sent ({message.Data.Subject}) to: {message.Data.To}");
             }
             catch (Exception e)
             {
                 e.Log(null, "Enviando mail key GeneralMailSender");
                 var exmsg = $"Enviando mail : {e.Message}\nStackTrace:{e.StackTrace}";
-                Console.WriteLine(exmsg, "Error");
+                Console.WriteLine(exmsg, @"Error");
                 if (message.DequeueCount < 20)
                 {
                     throw;
