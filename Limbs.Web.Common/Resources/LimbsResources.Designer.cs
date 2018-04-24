bb789bdb6b0ab1df3999c565ff67fab2ea3d1075
Micaela Yanigro <micayanigro@gmail.com>
2018-04-24 12:50:01 -0300

    -Cambio en mensaje de contraseña incorrecta -Cambio en mail de bienvenida a embajadores

--- Limbs.Web.Common/Resources/LimbsResources.Designer.cs
+++ Limbs.Web.Common/Resources/LimbsResources.Designer.cs
@@ -39,7 +39,7 @@ namespace Limbs.Web.Common.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager(typeof(LimbsResources));
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Limbs.Web.Common.Resources.LimbsResources", typeof(LimbsResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -160,7 +160,7 @@ namespace Limbs.Web.Common.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to PasswordMismatch.
+        ///   Looks up a localized string similar to La contraseña actual es incorrecta.
         /// </summary>
         public static string PasswordMismatch {
             get {
