2e1a84ecb4898dd51520f00c08752ad12fcde70b
Alejandro Banzas <alebanzas@outlook.com>
2018-09-18 19:22:15 -0700

    search orders in admin

---
+++ Limbs.Web/Areas/Admin/Models/OrderListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Limbs.Web.Entities.Models;
+using Limbs.Web.Services;
+
+namespace Limbs.Web.Areas.Admin.Models
+{
+    public class OrderListViewModel
+    {
+        public IEnumerable<OrderModel> List { get; set; }
+        public OrderFilters Filters { get; set; }
+    }
+}
