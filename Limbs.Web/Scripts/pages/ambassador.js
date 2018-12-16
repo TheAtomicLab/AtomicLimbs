1117e270b0940325c8728ecd63c976453bbfffda
mpetrini <dev.mpetrini@gmail.com>
2018-12-16 18:38:36 -0300

    printer required details

--- Limbs.Web/Scripts/pages/ambassador.js
+++ Limbs.Web/Scripts/pages/ambassador.js
@@ -1,7 +1,4 @@
 $(document).ready(function () {
-
-    //$("[name='ambassadorPrinter']").hide();
-
     let organization = $('#Organization');
     let organizationName = $('#OrganizationName');
     let roleInOrganization = $('#RoleInOrganization');
@@ -11,27 +8,6 @@ $(document).ready(function () {
     if (organization.val() !== 'CuentaPropia') {
         organizationBox.show();
     }
-
-    //$('[name="havePrinter"]').on("change", function () {
-    //    havePrinter($(this).val());
-    //});
-    let printingArea = $('#Printer_PrintingArea');
-    let printerWidth = $('#Printer_Width');
-    let printerLong = $('#Printer_Long');
-    let printerHeight = $('#Printer_Height');
-
-    printingArea.on('input', function () {
-        printerWidth.val('').addClass('ignore');
-        printerLong.val('').addClass('ignore');
-        printerHeight.val('').addClass('ignore');
-
-        $(this).removeClass('ignore');
-    });
-
-    $(printerHeight, printerWidth, printerLong).on('input', function () {
-        printingArea.val('').addClass('ignore');
-        $(this).removeClass('ignore');
-    });
 
     $('#Organization').change(function () {
         if ($(this).val() === 'CuentaPropia') {
@@ -44,8 +20,8 @@ $(document).ready(function () {
         }
     });
 
-    var settngs = $.data($('.form')[0], 'validator').settings;
-    settngs.ignore = ".ignore";
+    //var settngs = $.data($('.form')[0], 'validator').settings;
+    //settngs.ignore = ".ignore";
 
     frm.submit(function (e) {
         if (!$(this).valid()) {
@@ -55,29 +31,3 @@ $(document).ready(function () {
         }
     });
 });
-
-
-//function havePrinter(value) {
-//    if (value != "true") {
-//        hidePrinter();
-//    } else {
-//        showPrinter();
-//    }
-//}
-
-//function hidePrinter() {
-//    $("[name='ambassadorPrinter']").hide();
-
-//    $("[name='Printer.Brand']").val('');
-//    $("[name='Printer.Model']").val('');
-//    $("[name='Printer.Width']").val('');
-//    $("[name='Printer.Long']").val('');
-//    $("[name='Printer.Height']").val('');
-//    $("[name='Printer.PrintingArea']").val('');
-
-//    $("[name='Printer.IsHotBed']").prop('checked', false);
-//}
-
-//function showPrinter() {
-//    $("[name='ambassadorPrinter']").show();
-//}
