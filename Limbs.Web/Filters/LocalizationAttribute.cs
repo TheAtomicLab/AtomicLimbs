4797a67f6f2cad2074a5572bc6afda19e3f2e3d5
mlmicozzi <ing.mlmicozzi@gmail.com>
2019-06-09 16:33:59 -0300

    Change language in the Url

---
+++ Limbs.Web/Filters/LocalizationAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Limbs.Web.Helpers;
+
+namespace Limbs.Web.Filters
+{
+    public class LocalizationAttribute : ActionFilterAttribute
+    {
+        private string _DefaultLanguage = "es";
+        //private string[] allowedLanguages = { "nl", "en" };
+
+        public LocalizationAttribute(string defaultLanguage)
+        {
+            _DefaultLanguage = defaultLanguage;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            string lang = (string)filterContext.RouteData.Values["lang"] ?? _DefaultLanguage;
+            LanguageHelper.SetLanguage(lang);
+        }
+    }
+}
