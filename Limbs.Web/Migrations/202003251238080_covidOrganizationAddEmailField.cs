01719e5ce089fa6923f0ca20c21ad67358d0e295
mlmicozzi <ing.mlmicozzi@gmail.com>
2020-03-26 22:09:28 -0300

    -

--- Limbs.Web/Migrations/202003251238080_covidOrganizationAddEmailField.cs
+++ Limbs.Web/Migrations/202003251238080_covidOrganizationAddEmailField.cs
@@ -8,11 +8,19 @@ namespace Limbs.Web.Migrations
         public override void Up()
         {
             AddColumn("dbo.CovidOrganizationModels", "Email", c => c.String());
+            AddColumn("dbo.CovidOrganizationModels", "OrganizationPhone", c => c.String());
+            AddColumn("dbo.CovidOrganizationModels", "OrganizationPhoneIntern", c => c.String());
+            AddColumn("dbo.CovidOrganizationModels", "PersonalPhone", c => c.String());
+            AddColumn("dbo.CovidOrganizationModels", "Dni", c => c.String());
         }
         
         public override void Down()
         {
             DropColumn("dbo.CovidOrganizationModels", "Email");
+            DropColumn("dbo.CovidOrganizationModels", "PersonalPhone");
+            DropColumn("dbo.CovidOrganizationModels", "OrganizationPhone");
+            DropColumn("dbo.CovidOrganizationModels", "OrganizationPhoneIntern");
+            DropColumn("dbo.CovidOrganizationModels", "Dni");
         }
     }
 }
